849f531f514319794dc177a03ceb4d850cd562d7
JFC1183-MiguelAlvarez <migalvray@alum.us.es>
2025-03-03 17:20:22 +0100

    task 'Flight crew members entity' (relationship added) completed!

--- src/main/java/acme/entities/flightCrewMember/FlightCrewMember.java
+++ src/main/java/acme/entities/flightCrewMember/FlightCrewMember.java
@@ -13,6 +13,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airline.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,42 +25,38 @@ public class FlightCrewMember extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
+	@Automapped
+	@Valid
+	Airline						airline;
+
+	@Mandatory
 	@Column(unique = true)
 	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
-	String						employeeCode;
+	private String				employeeCode;
 
 	@Mandatory
 	@Automapped
 	@ValidString(pattern = "^+?\\d{6,15}$")
-	String						phoneNumber;
+	private String				phoneNumber;
 
 	@Mandatory
 	@Automapped
 	@ValidString(min = 1, max = 255)
-	String						languageSkills;
+	private String				languageSkills;
 
 	@Mandatory
 	@Automapped
 	@Valid
-	AvailabilityStatus			availabilityStatus;
-
-	/*
-	 * @Mandatory
-	 * 
-	 * @Automapped
-	 * 
-	 * @Valid
-	 * Airline airline;
-	 */
+	private AvailabilityStatus	availabilityStatus;
 
 	@Mandatory
 	@Automapped
 	@ValidMoney
-	Money						salary;
+	private Money				salary;
 
 	@Optional
 	@Automapped
-	@ValidNumber(min = 1, max = 150) // no person can have more than 150 years of experience
-	Integer						yearsOfExperience;
+	@ValidNumber(min = 0, max = 150) // no person can have more than 150 years of experience
+	private Integer				yearsOfExperience;
 
 }
