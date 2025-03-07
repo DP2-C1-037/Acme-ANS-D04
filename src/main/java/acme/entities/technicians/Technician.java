78cb743f75c3127f934c540a525fbb570f4267f6
Ultimate88x <alejandrogonzalez88x@gmail.com>
2025-03-07 23:49:27 +0100

    Add Technician Entity

---
+++ src/main/java/acme/entities/technicians/Technician.java
@@ -0,0 +1,65 @@
+
+package acme.entities.technicians;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.principals.UserAccount;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Technician extends AbstractEntity {
+
+	// Serialisation version -----------------------------------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes ----------------------------------------------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				licenseNumber;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				specialisation;
+
+	@Mandatory
+	@Automapped
+	private Boolean				healthTestPassed;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 120)
+	@Automapped
+	private Integer				experienceYears;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				certifications;
+
+	// Relationships ----------------------------------------------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@OneToOne
+	private UserAccount			userAccount;
+}
