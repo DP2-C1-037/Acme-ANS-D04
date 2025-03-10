350795caf1594a230924b2208258b52b0d7eea37
Ignacio0407 <igngutser@alum.us.es>
2025-03-10 17:51:35 +0100

    fix: :ambulance: Corrected query so that project could boot again

--- src/main/java/acme/entities/airline/LegRepository.java
+++ src/main/java/acme/entities/airline/LegRepository.java
@@ -10,17 +10,14 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface LegRepository extends AbstractRepository {
 
-	/*
-	 * @Query("SELECT new acme.entities.airline.FlightLegInfo(l.scheduledDeparture, l.departureAirport.city) FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC LIMIT 1")
-	 * 
-	 * public FlightLegInfo flightDepartureDateAndCity(@Param("flightId") Integer flightId);
-	 * 
-	 * @Query("SELECT new acme.entities.airline.FlightLegInfo(l.scheduledArrival, l.arrivalAirport.city) FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledArrival DESC LIMIT 1")
-	 * 
-	 * public FlightLegInfo flightArrivalDateAndCity(@Param("flightId") Integer flightId);
-	 */
+	//	 @Query("SELECT new acme.entities.airline.FlightLegInfo(l.scheduledDeparture, l.departureAirport.city) FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC LIMIT 1")
+	//	  
+	//	 public FlightLegInfo flightDepartureDateAndCity(@Param("flightId") Integer flightId);
+	//	  
+	//	 @Query("SELECT new acme.entities.airline.FlightLegInfo(l.scheduledArrival, l.arrivalAirport.city) FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledArrival DESC LIMIT 1")
+	//	 
+	//	 public FlightLegInfo flightArrivalDateAndCity(@Param("flightId") Integer flightId);
 
-	@Query("SELECT new acme.entities.airline.FlightLegInfo(firstLeg.scheduledDeparture, firstLeg.departureAirport.city, lastLeg.scheduledArrival, lastLeg.arrivalAirport.city) FROM Leg firstLeg, Leg lastLeg WHERE firstLeg.flight.id = :flightId AND lastLeg.flight.id = :flightId AND firstLeg.scheduledDeparture = (SELECT MIN(l.scheduledDeparture) FROM Leg l WHERE l.flight.id = :flightId) AND lastLeg.scheduledArrival = (SELECT MAX(l.scheduledArrival) FROM Leg l WHERE l.flight.id = :flightId)")
-	public FlightLegInfo flightData(@Param("flightId") Integer flightId);
-
+	@Query("SELECT NEW acme.entities.airline.FlightLegInfo(MIN(l.scheduledDeparture), (SELECT a.city FROM Leg l2 JOIN l2.departureAirport a WHERE l2.flight.id = :flightId AND l2.scheduledDeparture = MIN(l.scheduledDeparture)), MAX(l.scheduledArrival), (SELECT a.city FROM Leg l3 JOIN l3.arrivalAirport a WHERE l3.flight.id = :flightId AND l3.scheduledArrival = MAX(l.scheduledArrival))) FROM Leg l WHERE l.flight.id = :flightId")
+	FlightLegInfo flightData(@Param("flightId") Integer flightId);
 }
