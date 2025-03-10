5c47f9f25110621e82e5df21619af06e6f18eade
Ignacio0407 <igngutser@alum.us.es>
2025-03-10 23:30:28 +0100

    feat: :sparkles: Added entity for Airline manager's dashboard

---
+++ src/main/java/acme/entities/airline/AirlineManager.java
@@ -0,0 +1,52 @@
+
+package acme.entities.airline;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidIdentifierNumber;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@ValidIdentifierNumber
+public class AirlineManager extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Column(unique = true)
+	@ValidString(min = 8, max = 9, pattern = "[A-Z]{2,3}\\d{6}$")
+	@Automapped
+	// the first two or three letters correspond to their initials -> @ValidIdentifierNumber
+	private String				identifierNumber;
+
+	@Mandatory
+	@ValidNumber
+	@Automapped
+	private Integer				experienceYears;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	private Date				birthDate;
+
+	// MUST BE STORED SOMEWHERE ELSE
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				pictureLink;
+
+}
---
+++ src/main/java/acme/entities/airline/Flight.java
@@ -0,0 +1,75 @@
+
+package acme.entities.airline;
+
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.helpers.SpringHelper;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Flight extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				tag;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 1)
+	@Automapped
+	private Integer				requiresSelfTransfer;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				cost;
+
+	@Optional
+	@ValidString
+	@Automapped
+	private String				description;
+
+	//	@Transient
+	//	public FlightLegInfo getScheduledDepartureAndOriginCity() {
+	//		LegRepository legRepository = SpringHelper.getBean(LegRepository.class);
+	//		return legRepository.flightArrivalDateAndCity(this.getId());
+	//	}
+	//
+	//	@Transient
+	//	public FlightLegInfo getScheduledArrivalAndDestinyCity() {
+	//		LegRepository legRepository = SpringHelper.getBean(LegRepository.class);
+	//		return legRepository.flightDepartureDateAndCity(this.getId());
+	//	}
+
+
+	@Transient
+	public FlightLegInfo getArrivalAndOriginData() {
+		LegRepository legRepository = SpringHelper.getBean(LegRepository.class);
+		return legRepository.flightData(this.getId());
+	}
+
+	@Transient
+	private Integer getLayoversNumber() {
+		LegRepository legRepository = SpringHelper.getBean(LegRepository.class);
+		return legRepository.findAll().size();
+	}
+
+	//	@ManyToOne
+	//	@JoinColumn(name = "weather_conditions_id")
+	//	private WeatherConditions weatherConditions;
+
+}
