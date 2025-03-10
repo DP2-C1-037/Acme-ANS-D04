93699870a35054e1355c02ba5a8822ac3373b48a
JFC1183-MiguelAlvarez <migalvray@alum.us.es>
2025-03-10 17:17:41 +0100

    task 'Flight crew member dashboards' (contraints added). completed!

---
+++ src/main/java/acme/constraints/FlightAssignmentsStatusValidator.java
@@ -0,0 +1,48 @@
+
+package acme.constraints;
+
+import java.util.Map;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.entities.flightAssignment.AssignmentStatus;
+
+@Validator
+public class FlightAssignmentsStatusValidator extends AbstractValidator<ValidFlightAssignmentsStatus, Map<String, Integer>> {
+
+	@Override
+	protected void initialise(final ValidFlightAssignmentsStatus annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final Map<String, Integer> value, final ConstraintValidatorContext context) {
+		assert context != null;
+
+		if (value == null || value.isEmpty())
+			return false;
+
+		boolean result = true;
+
+		for (String key : value.keySet()) {
+			boolean isValidStatus = this.isValidAssignmentStatus(key);
+			if (!isValidStatus) {
+				super.state(context, false, "flightAssignments", "acme.validation.assignments-status.message");
+				result = false;
+			}
+		}
+
+		return result;
+	}
+
+	private boolean isValidAssignmentStatus(final String status) {
+		try {
+			AssignmentStatus.valueOf(status);
+			return true;
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+	}
+}
--- src/main/java/acme/forms/FlightCrewMemberDashboard.java
+++ src/main/java/acme/forms/FlightCrewMemberDashboard.java
@@ -4,7 +4,14 @@ package acme.forms;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.constraints.Size;
+
 import acme.client.components.basis.AbstractForm;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidScore;
+import acme.constraints.ValidFlightAssignmentsStatus;
+import acme.constraints.ValidIncidentCounts;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,16 +21,35 @@ public class FlightCrewMemberDashboard extends AbstractForm {
 
 	private static final long		serialVersionUID	= 1L;
 
+	@Mandatory
+	@Size(min = 1)
 	private List<String>			lastFiveDestinations;
 
+	@Mandatory
+	@ValidIncidentCounts
 	private Map<String, Integer>	incidentCountsBySeverity; // Example: {"0-3": 5, "4-7": 2, "8-10": 1}
 
+	@Mandatory
+	@Size(min = 1)
 	private List<String>			lastLegCrewMembers;
 
+	@Mandatory
+	@ValidFlightAssignmentsStatus
 	private Map<String, Integer>	flightAssignmentsByStatus;
 
+	@Mandatory
+	@ValidScore
 	private Double					averageFlightAssignments;
+
+	@Mandatory
+	@ValidNumber(min = 0)
 	private Integer					minFlightAssignments;
+
+	@Mandatory
+	@ValidNumber(min = 0)
 	private Integer					maxFlightAssignments;
+
+	@Mandatory
+	@ValidScore
 	private Double					stdDevFlightAssignments;
 }
