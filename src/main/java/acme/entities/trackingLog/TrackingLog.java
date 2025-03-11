b1a31685196ba97f1b8f4f3467215c56834c7d16
Salma El Hakimy <yosalma2003@gmail.com>
2025-03-11 02:33:55 +0100

    Tracking Log entity added

--- src/main/java/acme/entities/trackingLog/TrackingLog.java
+++ src/main/java/acme/entities/trackingLog/TrackingLog.java
@@ -1,5 +1,5 @@
 
-package acme.entities.claim;
+package acme.entities.trackingLog;
 
 import java.util.Date;
 
@@ -12,18 +12,17 @@ import javax.validation.Valid;
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
-import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
-import acme.datatypes.ClaimType;
-import acme.entities.assistanceAgents.AssistanceAgent;
+import acme.entities.claim.Claim;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Claim extends AbstractEntity {
+public class TrackingLog extends AbstractEntity {
 	// Serialisation version -----------------------------------------------------------------------------------------
 
 	private static final long	serialVersionUID	= 1L;
@@ -33,32 +32,32 @@ public class Claim extends AbstractEntity {
 	@Mandatory
 	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				registrationMoment;
+	private Date				lastUpdateMoment;
 
 	@Mandatory
-	@ValidEmail
+	@ValidString(min = 1, max = 50)
 	@Automapped
-	private String				passengerEmail;
+	private String				step;
 
 	@Mandatory
-	@ValidString(min = 1, max = 255)
+	@ValidNumber(min = 0, max = 100)
 	@Automapped
-	private String				description;
-
-	@Mandatory
-	@Automapped
-	@Valid
-	private ClaimType			type;
+	private Double				resolPercentage;
 
 	@Mandatory
 	@Valid
 	@Automapped
 	private Boolean				accepted;
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	private String				resolution;
+
 	// Relationships ----------------------------------------------------------------------------------------------------
 
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
-	private AssistanceAgent		assistantAgent;
-
+	private Claim				claim;
 }
