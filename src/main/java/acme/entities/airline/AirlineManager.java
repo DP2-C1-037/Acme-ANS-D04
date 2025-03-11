3f1602d087def124ad0a354633f1e0762354f5d7
Ignacio0407 <igngutser@alum.us.es>
2025-03-11 18:13:05 +0100

    refactor: :art: Added relation to airline and modified validator to check only first 2 initials

--- src/main/java/acme/entities/airline/AirlineManager.java
+++ src/main/java/acme/entities/airline/AirlineManager.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
@@ -49,5 +51,12 @@ public class AirlineManager extends AbstractRole {
 	@ValidUrl
 	@Automapped
 	private String				pictureLink;
+
+	// Relationships
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Airline				airline;
 
 }
