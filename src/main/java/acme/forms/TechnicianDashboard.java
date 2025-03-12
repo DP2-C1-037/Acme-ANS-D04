3fe2a629b7514506be3a801beb687f15a918fc64
Ultimate88x <alejandrogonzalez88x@gmail.com>
2025-03-12 19:49:21 +0100

    Remove unnecessary @s

--- src/main/java/acme/forms/TechnicianDashboard.java
+++ src/main/java/acme/forms/TechnicianDashboard.java
@@ -4,12 +4,7 @@ package acme.forms;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.Valid;
-
 import acme.client.components.basis.AbstractForm;
-import acme.client.components.validation.Mandatory;
-import acme.client.components.validation.Optional;
-import acme.client.components.validation.ValidNumber;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,46 +18,16 @@ public class TechnicianDashboard extends AbstractForm {
 
 	// Attributes -------------------------------------------------------------
 
-	@Mandatory
 	private Map<String, Integer>	maintenanceRecordsByStatus;
-
-	@Optional
-	@Valid
 	private String					nearestInspectionMaintenanceRecord;
-
-	@Mandatory
 	private List<String>			higherTaskNumberAircrafts;
-
-	@Optional
-	@ValidNumber(min = 0)
 	private Double					lastYearAverageMaintenanceCost;
-
-	@Optional
-	@ValidNumber(min = 0)
 	private Double					lastYearMaxMaintenanceCost;
-
-	@Optional
-	@ValidNumber(min = 0)
 	private Double					lastYearMinMaintenanceCost;
-
-	@Optional
-	@ValidNumber(min = 0)
 	private Double					lastYearStandardDeviationMaintenanceCost;
-
-	@Mandatory
-	@ValidNumber(min = 0)
 	private Double					taskAverageDuration;
-
-	@Mandatory
-	@ValidNumber(min = 0)
 	private Integer					taskMaxDuration;
-
-	@Mandatory
-	@ValidNumber(min = 0)
 	private Integer					tasgMinDuration;
-
-	@Mandatory
-	@ValidNumber(min = 0)
 	private Double					taskStandardDeviationDuration;
 
 }
