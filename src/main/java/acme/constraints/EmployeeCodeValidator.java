4a0c8b8ff61178c2199fd9f61c138f2afb21eb7d
Salma El Hakimy <yosalma2003@gmail.com>
2025-03-13 14:39:27 +0100

    Dashboard added

--- src/main/java/acme/constraints/EmployeeCodeValidator.java
+++ src/main/java/acme/constraints/EmployeeCodeValidator.java
@@ -1,37 +1,57 @@
 
 package acme.constraints;
 
-import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import acme.client.components.principals.DefaultUserIdentity;
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
 import acme.realms.AssistanceAgent;
 
-public class EmployeeCodeValidator implements ConstraintValidator<ValidEmployeeCode, AssistanceAgent> {
+@Validator
+public class EmployeeCodeValidator extends AbstractValidator<ValidEmployeeCode, AssistanceAgent> {
 
 	@Override
-	public void initialize(final ValidEmployeeCode constraintAnnotation) {
+	protected void initialise(final ValidEmployeeCode constraintAnnotation) {
 		assert constraintAnnotation != null;
 	}
 
 	@Override
-	public boolean isValid(final AssistanceAgent value, final ConstraintValidatorContext context) {
-		if (value == null || value.getEmployeeCode() == null || value.getUserAccount() == null)
-			return false;
+	public boolean isValid(final AssistanceAgent assistanceAgent, final ConstraintValidatorContext context) {
+		assert context != null;
 
-		String employeeCode = value.getEmployeeCode();
-		String name = value.getUserAccount().getIdentity().getFullName();
+		boolean result;
+		boolean isNull;
+		isNull = assistanceAgent == null || assistanceAgent.getEmployeeCode() == null;
 
-		if (!employeeCode.matches("^[A-Z]{2,3}\\d{6}$"))
-			return false;
+		if (!isNull) {
+			String employeeCode = assistanceAgent.getEmployeeCode();
+			boolean matchesPattern = employeeCode.matches("^[A-Z]{2,3}\\d{6}$");
 
-		String initialsFromCode = employeeCode.substring(0, employeeCode.length() == 9 ? 3 : 2);
+			super.state(context, matchesPattern, "employeeCode", "{acme.validation.employee-code.invalid-format.message}");
 
-		String[] nameParts = name.split(" ");
-		StringBuilder initialsFromName = new StringBuilder();
-		for (String part : nameParts)
-			if (!part.isEmpty())
-				initialsFromName.append(part.charAt(0));
+			if (matchesPattern) {
+				DefaultUserIdentity identity = assistanceAgent.getUserAccount().getIdentity();
 
-		return initialsFromCode.equals(initialsFromName.toString().toUpperCase());
+				String nameInitial = String.valueOf(identity.getName().charAt(0)).toUpperCase();
+				String firstNameInitial = String.valueOf(identity.getSurname().charAt(0)).toUpperCase();
+				String secondNameInitial = "";
+				Integer initialsLength = 2;
+
+				if (identity.getSurname().contains(" ")) {
+					secondNameInitial = String.valueOf(identity.getSurname().split(" ")[1].charAt(0)).toUpperCase();
+					initialsLength++;
+				}
+
+				String expectedInitials = nameInitial + firstNameInitial + secondNameInitial;
+				String employeeCodeInitials = employeeCode.substring(0, initialsLength);
+
+				boolean initialsMatch = expectedInitials.equals(employeeCodeInitials);
+				super.state(context, initialsMatch, "licenseNumber", "{acme.validation.employee-code.not-matching-initials.message}");
+			}
+		}
+
+		result = !super.hasErrors(context);
+		return result;
 	}
 }
