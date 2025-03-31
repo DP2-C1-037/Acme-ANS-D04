0a85860a93fd1cbede8bbdda8215997193b794b9
AdrianChabrera <adrianchabrera@gmail.com>
2025-03-31 21:15:16 +0200

    fixed show booking assignations bug

--- src/main/java/acme/features/customer/assignedTo/AssignedToRepository.java
+++ src/main/java/acme/features/customer/assignedTo/AssignedToRepository.java
@@ -29,8 +29,14 @@ public interface AssignedToRepository extends AbstractRepository {
 	@Query("select b from Booking b where b.draftMode = true and b.customer.id = :customerId")
 	Collection<Booking> findAllNotPublishedBookingsFromCustomerId(final int customerId);
 
+	@Query("select b from Booking b where b.customer.id = :customerId")
+	Collection<Booking> findAllBookingsFromCustomerId(final int customerId);
+
 	@Query("select p from Passenger p where p.draftMode = true and p.customer.id = :customerId")
 	Collection<Passenger> findAllNotPublishedPassengersFromCustomerId(final int customerId);
+
+	@Query("select p from Passenger p where p.customer.id = :customerId")
+	Collection<Passenger> findAllPassengersFromCustomerId(final int customerId);
 
 	@Query("select at from AssignedTo at where at.booking.id = :bookingId and at.passenger.id = :passengerId")
 	Collection<AssignedTo> findAssignationFromBookingIdAndPassengerId(final int bookingId, final int passengerId);
