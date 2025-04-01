0836c3c3899b4a9676fbdb78c53153d53615dcea
Salma El Hakimy <yosalma2003@gmail.com>
2025-04-01 15:52:05 +0200

    Change AssistanceAgentRepository route

--- src/main/java/acme/realms/assistanceAgent/AssistanceAgentRepository.java
+++ src/main/java/acme/realms/assistanceAgent/AssistanceAgentRepository.java
@@ -1,11 +1,10 @@
 
-package acme.features.assistanceAgent;
+package acme.realms.assistanceAgent;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
-import acme.realms.AssistanceAgent;
 
 @Repository
 public interface AssistanceAgentRepository extends AbstractRepository {
