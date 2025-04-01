4b033e84502b97a95c4037f7d88f8ac558c9f29b
AdrianChabrera <adrianchabrera@gmail.com>
2025-04-01 22:10:25 +0200

    listing booking assignations functionality changed

--- src/main/java/acme/features/customer/assignedTo/CustomerAssignedToListService.java
+++ src/main/java/acme/features/customer/assignedTo/CustomerAssignedToListService.java
@@ -8,18 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
+import acme.entities.booking.Booking;
 import acme.entities.mappings.AssignedTo;
 import acme.realms.Customer;
 
 @GuiService
 public class CustomerAssignedToListService extends AbstractGuiService<Customer, AssignedTo> {
 
-	// Internal state ---------------------------------------------------------
+	// Internal state ------------------------------------------------------------
 
 	@Autowired
 	private CustomerAssignedToRepository repository;
 
-	// AbstractGuiService interface -------------------------------------------
+	// AbstractGuiService interface ----------------------------------------------
 
 
 	@Override
@@ -30,19 +31,32 @@ public class CustomerAssignedToListService extends AbstractGuiService<Customer, 
 	@Override
 	public void load() {
 		Collection<AssignedTo> assignedTos;
-		int customerId;
+		int masterId;
 
-		customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
-		assignedTos = this.repository.findAssignedTosByCustomerId(customerId);
+		masterId = super.getRequest().getData("masterId", int.class);
+		assignedTos = this.repository.findAssignedTosByBookingId(masterId);
 
 		super.getBuffer().addData(assignedTos);
+	}
+
+	@Override
+	public void unbind(final Collection<AssignedTo> assignedTos) {
+		int masterId;
+		Booking booking;
+
+		masterId = super.getRequest().getData("masterId", int.class);
+		booking = this.repository.findBookingById(masterId);
+
+		super.getResponse().addGlobal("masterId", masterId);
+		super.getResponse().addGlobal("draftMode", booking.isDraftMode());
 	}
 
 	@Override
 	public void unbind(final AssignedTo assignedTo) {
 		Dataset dataset;
 
-		dataset = super.unbindObject(assignedTo, "booking.locatorCode", "passenger.passportNumber");
+		dataset = super.unbindObject(assignedTo, "passenger.fullName", "passenger.email", "passenger.passportNumber");
+
 		super.getResponse().addData(dataset);
 	}
 }
