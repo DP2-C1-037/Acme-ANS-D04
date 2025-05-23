424c97da7a4040946120eb4593f0ab41954903cd
Ultimate88x <alejandrogonzalez88x@gmail.com>
2025-05-23 20:48:35 +0200

    Add Indexes from InvolvedIn Repository

--- src/main/java/acme/entities/involvedIn/InvolvedIn.java
+++ src/main/java/acme/entities/involvedIn/InvolvedIn.java
@@ -2,7 +2,9 @@
 package acme.entities.involvedIn;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -15,6 +17,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "maintenanceRecord_id, task_id")
+})
 public class InvolvedIn extends AbstractEntity {
 
 	// Serialisation version -----------------------------------------------------------------------------------------
