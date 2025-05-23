d70a62bbee3e05aca1da340853d8ec268ee4824e
Ultimate88x <alejandrogonzalez88x@gmail.com>
2025-05-23 18:39:42 +0200

    Unauthorise delete when using URL

--- src/main/java/acme/features/technician/involvedIn/TechnicianInvolvedInDeleteService.java
+++ src/main/java/acme/features/technician/involvedIn/TechnicianInvolvedInDeleteService.java
@@ -1,17 +1,12 @@
 
 package acme.features.technician.involvedIn;
 
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import acme.client.components.models.Dataset;
-import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.mappings.InvolvedIn;
 import acme.entities.tasks.Task;
-import acme.entities.tasks.TaskType;
 import acme.realms.technicians.Technician;
 
 @GuiService
@@ -31,11 +26,18 @@ public class TechnicianInvolvedInDeleteService extends AbstractGuiService<Techni
 		int id;
 		Technician technician;
 		InvolvedIn involvedIn;
+		String method;
 
-		id = super.getRequest().getData("id", int.class);
-		involvedIn = this.repository.findInvolvedInById(id);
-		technician = involvedIn == null ? null : involvedIn.getMaintenanceRecord().getTechnician();
-		status = involvedIn != null && involvedIn.getMaintenanceRecord().getDraftMode() && super.getRequest().getPrincipal().hasRealm(technician);
+		method = super.getRequest().getMethod();
+
+		status = !method.equals("GET");
+
+		if (status) {
+			id = super.getRequest().getData("id", int.class);
+			involvedIn = this.repository.findInvolvedInById(id);
+			technician = involvedIn == null ? null : involvedIn.getMaintenanceRecord().getTechnician();
+			status = involvedIn != null && involvedIn.getMaintenanceRecord().getDraftMode() && super.getRequest().getPrincipal().hasRealm(technician);
+		}
 
 		super.getResponse().setAuthorised(status);
 	}
@@ -76,28 +78,6 @@ public class TechnicianInvolvedInDeleteService extends AbstractGuiService<Techni
 
 	@Override
 	public void unbind(final InvolvedIn involvedIn) {
-		Collection<Task> tasks;
-		SelectChoices choices;
-		SelectChoices types;
-		Dataset dataset;
-		int id;
-
-		id = super.getRequest().getPrincipal().getActiveRealm().getId();
-
-		tasks = this.repository.findAllAvailableTasksForInvolvedIn(id, involvedIn.getMaintenanceRecord().getId());
-		tasks.add(involvedIn.getTask());
-		choices = SelectChoices.from(tasks, "description", involvedIn.getTask());
-
-		types = SelectChoices.from(TaskType.class, involvedIn.getTask().getType());
-
-		dataset = super.unbindObject(involvedIn, "task.technician.identity.name", "task.description", "task.priority", "task.estimatedDuration");
-		dataset.put("task", choices.getSelected().getKey());
-		dataset.put("tasks", choices);
-		dataset.put("types", types);
-
-		dataset.put("masterId", super.getRequest().getData("masterId", int.class));
-		dataset.put("draftMode", involvedIn.getMaintenanceRecord().getDraftMode());
-
-		super.getResponse().addData(dataset);
+		;
 	}
 }
